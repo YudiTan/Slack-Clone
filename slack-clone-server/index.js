5d663273643ede3572252d2241844f8bfe227818
Yudi Tan <yudi98@berkeley.edu>
2018-01-12 12:28:51 -0600

    completed v8 -- error validation

--- slack-clone-server/index.js
+++ slack-clone-server/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import path from 'path';
-import cors from 'cors';
-import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import { makeExecutableSchema } from 'graphql-tools';
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import cors from 'cors';
+
 import models from './models';
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
@@ -17,19 +18,27 @@ const schema = makeExecutableSchema({
 });
 
 const app = express();
+
 app.use(cors('*'));
 
-const graphqlEndpoint = '/graphql'
+const graphqlEndpoint = '/graphql';
+
+app.use(
+  graphqlEndpoint,
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      models,
+      user: {
+        id: 1,
+      },
+    },
+  }),
+);
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
-// bodyParser is needed just for POST.
-app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema, context: {
-  models,
-  user: {
-    id: 1
-  }
-}}));
 
-models.sequelize.sync().then(()=> {
-    app.listen(8080);
-})
+models.sequelize.sync({}).then(() => {
+  app.listen(8080);
+});
